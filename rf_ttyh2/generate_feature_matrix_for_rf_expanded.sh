3cd90cfb882f7a9ec25ca1f9a82e09342700be70
anna shcherbina <annashcherbina@gmail.com>
2018-09-28 10:40:02 -0700

    update rf feature extraction script for TTYH2 substrate

---
+++ rf_ttyh2/generate_feature_matrix_for_rf_expanded.sh
@@ -0,0 +1,15 @@
+#python generate_feature_matrix_for_rf_expanded.py --rna_lib_structure_summary_json /srv/scratch/annashch/adar_editing/data_from_xin/TTYH2_BC/1/rna_lib-structure-summary.json \
+#       --bpRNA_pickle /srv/scratch/annashch/adar_editing/bpRNA_wrapper/bpRNA.TTYH2_BC_1.pkl \
+#       --outf TTYH2_BC_1.features.txt
+
+#python generate_feature_matrix_for_rf_expanded.py --rna_lib_structure_summary_json /srv/scratch/annashch/adar_editing/data_from_xin/TTYH2_BC/2/rna_lib-structure-summary.json \
+#       --bpRNA_pickle /srv/scratch/annashch/adar_editing/bpRNA_wrapper/bpRNA.TTYH2_BC_2.pkl \
+#       --outf TTYH2_BC_2.features.txt
+
+#python generate_feature_matrix_for_rf_expanded.py --rna_lib_structure_summary_json /srv/scratch/annashch/adar_editing/data_from_xin/TTYH2_ECS_BC/1/rna_lib-structure-summary.json \
+#       --bpRNA_pickle /srv/scratch/annashch/adar_editing/bpRNA_wrapper/bpRNA.TTYH2_ECS_BC_1.pkl \
+#       --outf TTYH2_ECS_BC_1.features.txt
+
+python generate_feature_matrix_for_rf_expanded.py --rna_lib_structure_summary_json /srv/scratch/annashch/adar_editing/data_from_xin/TTYH2_ECS_BC/2/rna_lib-structure-summary.json \
+       --bpRNA_pickle /srv/scratch/annashch/adar_editing/bpRNA_wrapper/bpRNA.TTYH2_ECS_BC_2.pkl \
+       --outf TTYH2_ECS_BC_2.features.txt
