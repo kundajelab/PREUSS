ee245fe9212fa1c1e62c69fc3dd5e6512d5f1bf1
anna shcherbina <annashcherbina@gmail.com>
2018-09-28 09:55:59 -0700

    updated bootstrap annotation wrapper to handle case when no bootstrap data is available

---
+++ bpRNA_wrapper/bpRNA_wrapper.TTYH2.sh
@@ -0,0 +1,3 @@
+python bpRNA_wrapper.py --data_json /srv/scratch/annashch/adar_editing/data_from_xin/TTYH2_BC/1/rna_lib-structure-summary.json \
+       --pickle_out bpRNA.TTYH2_BC_1.pkl \
+       --text_out bpRNA.TTYH2_BC_1.txt
--- bpRNA_wrapper/bpRNA_wrapper.py
+++ bpRNA_wrapper/bpRNA_wrapper.py
@@ -27,27 +27,30 @@ def main():
     data=json.load(open(args.data_json,'r'))
     data_dict=dict()
     for item in data['items']:
-        #pdb.set_trace() 
-        bootstrap_structures=item['bootstrap_structures']
+        #pdb.set_trace()
         cur_id=item['rna_id']
         print("processing structure data for item:"+str(cur_id))
+        data_dict[cur_id]=dict()
         sequence_string=item['sequence_string']
         inferred_structure=item['inferred_structure']
-        data_dict[cur_id]=dict()
-        data_dict[cur_id]['bootstraps']=dict() 
-        #get a tally of how frequent each bootstrapped structure is
-        struct_tally=dict()
-        for struct in bootstrap_structures:
-            if struct not in struct_tally:
-                struct_tally[struct]=1
-            else:
-                struct_tally[struct]+=1
-        bootstrap_count=0
-        for struct in struct_tally:
-            header=','.join(['>bootstrap',cur_id+'.'+str(bootstrap_count),'frequency:'+str(struct_tally[struct])])
-            bpRNA_annotation=get_bpRNA_annotation('\n'.join([header,sequence_string,struct]))
-            bootstrap_count+=1
-            data_dict[cur_id]['bootstraps'][bpRNA_annotation]=struct_tally[struct]
+        try:
+            bootstrap_structures=item['bootstrap_structures']
+            data_dict[cur_id]['bootstraps']=dict()
+            struct_tally=dict()
+            #get a tally of how frequent each bootstrapped structure is
+            for struct in bootstrap_structures:
+                if struct not in struct_tally:
+                    struct_tally[struct]=1
+                else:
+                    struct_tally[struct]+=1
+            bootstrap_count=0
+            for struct in struct_tally:
+                header=','.join(['>bootstrap',cur_id+'.'+str(bootstrap_count),'frequency:'+str(struct_tally[struct])])
+                bpRNA_annotation=get_bpRNA_annotation('\n'.join([header,sequence_string,struct]))
+                bootstrap_count+=1
+                data_dict[cur_id]['bootstraps'][bpRNA_annotation]=struct_tally[struct]
+        except:
+            print("no bootstrap structures for "+str(cur_id)+", continuing")
         header=','.join(['>inferred',cur_id])
         bpRNA_annotation=get_bpRNA_annotation('\n'.join([header,sequence_string,inferred_structure]))
         data_dict[cur_id]['inferred']=bpRNA_annotation
@@ -60,10 +63,11 @@ def main():
         outf.write(">"+cur_id+',inferred\n')
         outf.write(data_dict[cur_id]['inferred']+'\n')
         i=0
-        for entry in data_dict[cur_id]['bootstraps']:
-            outf.write('>'+cur_id+'.'+str(i)+',bootstrap,count='+str(data_dict[cur_id]['bootstraps'][entry])+'\n')
-            outf.write(entry)
-            i+=1
+        if 'bootstraps' in data_dict[cur_id]: 
+            for entry in data_dict[cur_id]['bootstraps']:
+                outf.write('>'+cur_id+'.'+str(i)+',bootstrap,count='+str(data_dict[cur_id]['bootstraps'][entry])+'\n')
+                outf.write(entry)
+                i+=1
 
         
 if __name__=="__main__":
